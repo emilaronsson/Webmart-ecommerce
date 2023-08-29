afbfad9c4dcd847f3247b7ba4fa4f98e9b65c541
Emil Aronsson <aronsson.emil@hotmail.se>
2023-08-29 17:48:07 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,113 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {motion} from 'framer-motion'
+import products from '../assets/data/products'
+import Helmet from '../components/Helmet/Helmet';
+import '../styles/home.css'
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img.png'
+import Services from '../services/Services';
+import ProductsList from '../components/UI/ProductsList';
+import Clock from '../components/UI/Clock';
+import counterImg from '../assets/images/counter-timer-img.png';
+
+const Home = () => {
+
+    const [trendingProducts, setTrendingProducts] = useState([])
+    const [bestSellers, setBestSellers] = useState([])
+    const [newArrivals, setNewArrivals] = useState([])
+
+    const year = new Date().getFullYear()
+
+    useEffect(()=>{
+        const filteredTrendingProducts = products.filter(
+            (item) => item.category === 'chair'
+        );
+
+        const filteredBestSellers = products.filter(
+            (item) => item.category === 'sofa'
+        );
+
+        setTrendingProducts(filteredTrendingProducts);
+        setBestSellers(filteredBestSellers);
+    },[]);
+
+    return <Helmet title={'Home'}>
+        <section className='hero__section'>
+        <Container>
+            <Row>
+                <Col lg='6' md='6'>
+                    <div className='hero__content'>
+                        <p className='hero__subtitle'>Trending products in {year}</p>
+                        <h2>Make Your Interior More Minimalistic & Modern</h2>
+                        <p>lorem ipsum</p>
+                        <motion.button whileHover={{scale: 1.2}} 
+                            className='buy__btn'><Link to='/shop'>SHOP NOW</Link>
+                        </motion.button>
+                    </div>
+                </Col>
+                <Col lg='6' md='6'>
+                <div className='hero__img'>
+                    <img src={heroImg} alt='' />
+                </div>
+                </Col>
+            </Row>
+        </Container>
+        </section>
+        <Services />
+        <section className='trending__products'>
+            <Container>
+                <Row>
+                    <Col lg='12' className='text-center'>
+                        <h2 className='section__title'>Trending Products</h2>
+                    </Col>
+                    <ProductsList data={trendingProducts}/>
+                </Row>
+            </Container>
+        </section>
+
+        <section className='best__sellers'>
+        <Container>
+                <Row>
+                    <Col lg='12' className='text-center'>
+                        <h2 className='section__title'>Best Sellers</h2>
+                    </Col>
+                    <ProductsList data={bestSellers}/>
+                </Row>
+            </Container>
+        </section>
+
+        <section className='timer__count'>
+            <Container>
+                <Row>
+                    <Col lg='6' md='6'>
+                        <div className='clock__top-content'>
+                            <h4 className='text-white fs-6 mb-2'>Limited Offer</h4>
+                            <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+                        </div>
+                        <Clock />
+                        <motion.button whileTap={{scale: 1.2}} className='buy__btn store__btn'>
+                            <Link to="/shop">Visit Store</Link>
+                        </motion.button>
+                    </Col>
+
+                    <Col lg='6' md='6' className='text-end'>
+                        <img src={counterImg} alt='' />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+        <section className='new__arrivals'>
+            <Container>
+                <Row>
+                <Col lg='12' className='text-center'>
+                        <h2 className='section__title'>New Arrivals</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    </Helmet>
+}
+
+export default Home;
---
+++ src/services/Services.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
+import {motion} from 'framer-motion';
+import './services.css'
+import servicesData from '../assets/data/serviceData';
+const Services = () => {
+  return <section className='services'>
+    <Container>
+        <Row>
+
+        {
+            servicesData.map((item, index) =>(
+                <Col lg='3' md='4' key={index}>
+                <motion.div whileHover={{scale: 1.1}}
+                    className='service__item' 
+                    style={{background: item.bg}}>
+                    <span>
+                        <i class={item.icon}></i>
+                    </span>
+                    <div>
+                        <h3>{item.title}</h3>
+                        <p>{item.subtitle}</p>
+                    </div>
+                </motion.div>
+            </Col>
+            ))
+        }
+            
+        </Row>
+    </Container>
+  </section>
+}
+
+export default Services
